71c3bdcdb64f744bb0bd87d3424beba2cf646780
t1nguy3n <tng.one@gmail.com>
2019-04-26 18:52:37 -0500

    added textbook-page folder

---
+++ depaulSocial/src/app/textbook-page/textbook-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-textbook-page',
+  templateUrl: './textbook-page.component.html',
+  styleUrls: ['./textbook-page.component.css']
+})
+export class TextbookPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
